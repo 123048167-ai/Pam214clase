b509174a4221c46b18e2daeed138963d0bacba00
123048167-ai <123048167@upq.edu.mx>
2025-09-10 18:47:30 -0600

    avance javascript

---
+++ JS.ParaPrincipiantes/app.js
@@ -0,0 +1,25 @@
+document.write("<h1>hello world<\h1>");
+
+//tipos de datos
+"hello world"
+
+console.log("esto es un string");
+
+10000//numeros
+-2.3
+
+//boolean
+true
+false 
+
+//array
+['joe', 'isaac', 'jafet']
+[1,2,3]
+[true, false, true, false]
+
+ [
+    "username":isaac,
+    "score":0.3,
+    "hours":2,
+    "professional":false
+ ]
