c2a6f70f22233ffc136e090ca3d92c9f71b175e9
123048167-ai <123048167@upq.edu.mx>
2025-09-25 17:33:30 -0600

    ejercicioA

---
+++ Practica3/main.js
@@ -0,0 +1,5 @@
+const restar = require('./utils');
+console.log("6 - 3 =", restar(6, 3)); 
+console.log("10 - 7 =", restar(10, 7)); 
+console.log("15- 5 =", restar(15, 5)); 
+console.log("60 - 5 =", restar(60, 5)); 
